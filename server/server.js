5832016db17d1ef8844d3a8b447d99e368a9cca6
nuwrss <nuwrssazzam@gmail.com>
2020-11-03 21:36:06 +0200

    data file added

--- server/server.js
+++ server/server.js
@@ -3,6 +3,46 @@ const hostname = process.env.HOSTNAME || "localhost";
 const port = process.env.PORT || 4000;
 
 
-http.createServer.listen(port, hostname, () => {
+http.createServer(router).listen(port, hostname, () => {
   console.log(`Server running at port http://${hostname}:${port}`);
 });
+
+function router(request, response) {
+    const url = request.url;
+    const method = request.method;
+
+    if (method === "GET" && url ==="/"){
+        
+
+           const  arr = getNames(url.body)
+
+           response.end(arr)
+
+            
+        
+    }else{
+
+    }
+    
+  }
+
+
+  function getNames(body){
+
+    
+     read body from reqyuest 
+
+     filter ----
+
+     return arr ;
+
+  }
+
+
+var moviesName = [];
+reqad file Data.txt
+
+
+
+
+ 
